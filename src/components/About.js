939b322b2c523ec55864d497690abdd0775f8a7f
Mai Mohamed <maimohammed928@gmail.com>
2024-10-20 12:40:14 +0300

    add colors

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 const About =()=>{
     return(
 
-        <div>
+        <div className="bg-background">
             <p>About</p>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,18 @@ module.exports = {
     extend: {
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
+        foreground: "var(--foreground)", // Adding the foreground variable
+        color_1: "var(--color_1)",
+        color_2: "var(--color_2)",
+        color_3: "var(--color_3)",
+        color_4: "var(--color_4)",
       },
     },
   },
   plugins: [],
 };
+
+// rgba(42, 91, 160, 1)
+// rgba(24, 176, 62, 1)
+// rgba(255,255,255,1)
+// rgba(221, 221, 221, 1)
