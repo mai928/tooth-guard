54d2de2c8c09d9b9a7e5bc435b8530d40e7ddd01
Mai Mohamed <maimohammed928@gmail.com>
2024-10-20 12:27:40 +0300

    modify directory

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import About from "./components/About";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <div>
-<About/>
+      <About/>
     </div>
   );
 }
